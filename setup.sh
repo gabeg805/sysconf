0ed58660c71576ceecaea8f0bdca3fbb8844a7e0
gabeg805 <6011521+gabeg805@users.noreply.github.com>
2018-07-24 06:56:50 -0400

    Moved packages to its own script. Created a script to automatically update my git repos.

--- setup.sh
+++ setup.sh
@@ -20,7 +20,6 @@ PROJECT_DIR=$(readlink -e $(dirname "${0}"))
 ##
 CONF=
 GIT=
-PACKAGE=
 
 ##
 # Exit statuses.
@@ -34,9 +33,8 @@ EXIT_SETUP_DST_DOES_NOT_EXIST=12
 ##
 main()
 {
-	# Parse options
-	local short="hcgp"
-	local long="help,conf,git,package"
+	local short="hcg"
+	local long="help,conf,git"
 	local args=$(getopt -o "${short}" --long "${long}" --name "${PROJECT}" \
 					-- "${@}")
 	if [ $? -ne 0 ]
@@ -44,6 +42,14 @@ main()
 		usage
 		exit 1
 	fi
+
+	if [ $# -eq 0 ]
+	then
+		usage
+		exit 0
+	fi
+
+	# Parse options
 	eval set -- "${args}"
 
 	while true
@@ -59,9 +65,6 @@ main()
 			-g|--git)
 				GIT=true
 				;;
-			-p|--package)
-				PACKAGE=true
-				;;
 			*)
 				break
 				;;
@@ -76,14 +79,11 @@ main()
 	elif [ -n "${GIT}" ]
 	then
 		setup_git
-	elif [ -n "${PACKAGE}" ]
-	then
-		setup_packages
 	else
 		:
 	fi
 
-	exit $?
+	return $?
 }
 
 ##
@@ -102,9 +102,6 @@ usage()
 	echo 
 	echo "	  -g, --git"
 	echo "		  Download git repos."
-	echo 
-	echo "	  -p, --package"
-	echo "		  Install packages."
 }
 
 ##
@@ -137,6 +134,7 @@ setup_git()
 	then
 		return ${EXIT_SETUP_MAIN_PROJECT_DIR_DOES_NOT_EXIST}
 	fi
+
 	setup_git_personal
 	#setup_git_misc
 }
@@ -151,12 +149,15 @@ setup_git_personal()
 				 "wifi")
 	local r=
 	local e=
+
 	builtin cd "${HOME}/projects"
 	mkdir -pv bin
+
 	for r in "${repos[@]}"
 	do
 		git clone "ssh://git@github.com/gabeg805/${r}"
 		builtin cd bin
+
 		for e in "../${r}/"*
 		do
 			if [ -x "${e}" -a ! -d "${e}" ]
@@ -164,6 +165,7 @@ setup_git_personal()
 				ln -sv "${e}"
 			fi
 		done
+
 		builtin cd ..
 	done
 }
@@ -181,126 +183,6 @@ setup_git_misc()
 	git clone ssh://git@github.com/gabeg805/website
 }
 
-##
-# Install packages.
-##
-setup_packages()
-{
-	pacman -S acpi \
-		alsa-firmware \
-		alsa-oss \
-		alsa-plugins \
-		alsa-tools \
-		alsa-utils \
-		bc \
-		dmenu \
-		doxygen \
-		evince \
-		feh \
-		firefox \
-		gdb \
-		gimp \
-		git \
-		gparted \
-		gsimplecal \
-		i3-wm \
-		i3status \
-		ifplugd \
-		iproute2 \
-		iputils \
-		iw \
-		libpam-google-authenticator \
-		logrotate \
-		lsof \
-		moc \
-		nmap \
-		openssh \
-		openvpn \
-		os-prober \
-		otf-font-awesome \
-		pulseaudio \
-		pulseaudio-alsa \
-		python-gobject \
-		python-matplotlib \
-		qrencode \
-		redshift \
-		rsync \
-		rxvt-unicode \
-		scrot \
-		transmission-gtk \
-		ttf-anonymous-pro \
-		ttf-dejavu \
-		ttf-droid \
-		ttf-inconsolata \
-		unzip \
-		urxvt-perls \
-		vim \
-		vlc \
-		wget \
-		wpa_supplicant \
-		xbindkeys \
-		xcompmgr \
-		xorg-server \
-		xorg-fonts-misc \
-		xorg-xdm \
-		xorg-xev \
-		xorg-xhost \
-		xorg-xinit \
-		xorg-xkill \
-		xorg-xlsfonts \
-		xorg-xprop \
-		xorg-xwininfo \
-		zsh
-
-		# gohufont
-		# ttf-ms-fonts
-		# udisks
-		# youtube-dl-mp3
-		# xorg-server-utils
-
-		# pacman -S android-platform
-		# pacman -S android-sdk
-		# pacman -S android-sdk-build-tools
-		# pacman -S android-sdk-platform-tools
-		# pacman -S android-studio
-		# pacman -S arduino
-		# pacman -S arduino-mk
-		# pacman -S aspell
-		# pacman -S aspell-en
-		# pacman -S audacity
-		# pacman -S brother-mfc-j480dw
-		# pacman -S cups
-		# pacman -S device-mapper
-		# pacman -S incron
-		# pacman -S inetutils
-		# pacman -S inotify-tools
-		# pacman -S intltool
-		# pacman -S jdk7-openjdk
-		# pacman -S net-tools
-		# pacman -S openjdk7-src
-		# pacman -S pciutils
-		# pacman -S pcmciautils
-		# pacman -S python-pyserial
-		# pacman -S python2-matplotlib
-		# pacman -S python2-pyserial
-		# pacman -S python2-virtualenv
-		# pacman -S qiv
-		# pacman -S strace
-		# pacman -S sudo
-		# pacman -S sysfsutils
-		# pacman -S sysstat
-		# pacman -S systemd-sysvcompat
-		# pacman -S thunar
-		# pacman -S usbutils
-		# pacman -S util-linux
-		# pacman -S valgrind
-		# pacman -S vi
-		# pacman -S vpnc
-		# pacman -S wavpack
-		# pacman -S xdotool
-		# pacman -S xf86-input-synaptics
-		# pacman -S xfsprogs
-}
 
 ##
 # Create a symbolic link.
@@ -322,6 +204,7 @@ setup_mksym()
 	then
 		local msg="Replace '$(basename "${src}")' in '${dst}'? "
 		local response=
+
 		read -p "${msg}" response
 		case "${response}" in
 			y|Y) ln -svf "${src}" ;;
